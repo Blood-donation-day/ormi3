363e0d6cd483dfe3b6f85147ad2a363b2b7be4b2
Blood-donation-day <138690980+Blood-donation-day@users.noreply.github.com>
2023-08-24 17:31:06 +0900

    reduce를 사용하여 홀수 합 구하기
    배열에서 홀수와 짝수인 경우를 나누고  >> 필터로 결과값이 "홀수" 인 배열만 남기고 >> 남은 홀수 배열을 모두 더한다.

---
+++ JavaScript/홀수들의 합.js
@@ -0,0 +1,18 @@
+const homework = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function 홀짝분리기(int) {
+    let 홀짝판별기;
+    if (int%2===1){
+        홀짝판별기 = "홀수"
+    }else {
+        홀짝판별기 = "짝수"
+    }
+    return 홀짝판별기
+};
+
+console.log(homework.map(홀짝분리기)); // 홀짝 결과 
+const 홀수만 = homework.filter(num => 홀짝분리기(num) === "홀수"); // 홀수로 이루어진 출력
+const 홀수합계 = 홀수만.reduce((a, b) => a+b, 0);  //reduce로 `홀수만` 더하기
+
+console.log(홀수만);
+console.log(`홀수들의 합은 ${홀수합계}입니다.`);
