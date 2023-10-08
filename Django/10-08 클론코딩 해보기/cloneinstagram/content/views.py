a58490f387466d558c3f147c054a9a1e65056fe8
[KIMCHANGHWAN] <98susckdghks@naver.com>
2023-10-08 20:07:02 +0900

    장고 페이지 만들기 이미지 업로드
    인스타그램 클론코딩 해보기.
    모달창 열어서 이미지 업로드 하고 글작성 후 서버에 데이터 전송

--- Django/10-08 클론코딩 해보기/cloneinstagram/content/views.py
+++ Django/10-08 클론코딩 해보기/cloneinstagram/content/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
+
+class Main(APIView):
+    def get (self, request):
+        feed_list = Feed.objects.all().order_by('-id') # select * from content_feed / 아이디 인덱스의 역순으로
+    
+        return render(request, 'instagram/main.html', context=dict(feed_list=feed_list))
 
 # Create your views here.
+
